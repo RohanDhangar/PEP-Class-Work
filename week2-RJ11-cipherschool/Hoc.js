b5140dd82b1261f76ab436cbef264cd064347d6f
Rohan Dhangar <81848857+RohanDhangar@users.noreply.github.com>
2023-03-02 23:09:35 +0530

    Update Hoc.js

--- week2-RJ11-cipherschool/Hoc.js
+++ week2-RJ11-cipherschool/Hoc.js
@@ -2,9 +2,9 @@ const add = (a, b) => {
     return a + b;
   };
   
-//   const higherOrder = (a, referenceFunction) => {
-//     return referenceFunction(a, 20);
-//   };
+  const higherOrder = (a, referenceFunction) => {
+    return referenceFunction(a, 20);
+  };
   
   console.log(higherOrder(30, add));
   
